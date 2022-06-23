94e781d31fbb961c6b0a2d74cd72e8348c448706
lukiakos <lukiakos@gmail.com>
2022-06-23 14:57:53 +0200

    Solved the first 4 problems

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(string,num) {
+    i = 1
+    text = ''
+    if (num >= 0) {
+        while (i <= num) {
+        text += string
+        i += 1
+        }
+    } else {
+        return 'ERROR'
+    }
+    return text;
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    letters = []
+    output = ''
+    for (let i = 0; i < string.length; i++) {
+        letters.push(string[i])
+    }
+    for (let i = string.length - 1; i >= 0; i--){
+        output += letters[i]
+    }
+    return output
 };
 
 // Do not edit below this line
