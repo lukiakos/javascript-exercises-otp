820ef58f26c4f249d61922e05957101f4a8bc5c4
lukiakos <lukiakos@gmail.com>
2022-07-05 12:34:05 +0200

    Solved problems 8 and 9

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function(a,b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+ return numbers.reduce((sum, currValue) => sum + currValue, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(numbers) {
+  return numbers.reduce((sum, currValue) => sum * currValue, 1);
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  if (a == 0) {
+    return 1;
+  }
+  let total = 1;
+  while (a > 0) {
+    total += total * (a-1);
+    a -= 1;
+  }
+  return total;
 };
 
 // Do not edit below this line
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let cleanString = string.toLowerCase().replaceAll(" ", "").replaceAll(",", "").replaceAll(".","").replaceAll("!","")
+    let reverseString = cleanString.split("").reverse().join("");
+    return cleanString === reverseString ? true : false;
 };
 
 // Do not edit below this line
